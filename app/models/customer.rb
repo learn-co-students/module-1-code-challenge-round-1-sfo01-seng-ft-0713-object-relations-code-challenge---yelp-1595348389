e88cec1765874c7cdb7623b774f332bd12815e07
DariusWalker <dw862716@ohio.edu>
2020-07-21 11:11:06 -0700

    darius-walker-first-attempt

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,29 @@
+require 'pry'
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self 
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants 
+    Customer.all.uniq {|customers_who_reviewed| customers_who_reviewed.fullname == full_name}
+  end
+
+  def add_review(restaurant, rating)
+    
+  end
+
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Review
+    attr_reader :customer, :restaurant, :rating 
+
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer 
+        @restaurant = restaurant 
+        @rating = rating 
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer 
+        #unfinished 
+        Review.all.find  {|person| person.customer == customer}
+    end
+
+    def restuarant 
+        #unfinished
+        Review.all.find {|restaurant_reviewed| restaurant_reviewed.restaurant == restaurant}
+    end
+
+
+
+
   
 end
